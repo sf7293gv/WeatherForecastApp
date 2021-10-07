4bfca5b363b4010ef53b5db0dd97c2908e111d71
Sonic <sf7293gv@go.minnstate.edu>
2021-10-06 19:35:28 -0500

    Created a file to start working on the app

---
+++ weather_app.py
@@ -0,0 +1,13 @@
+"""Part 1: Weather Forecast  
+
+Use the forecast API to create a detailed, neatly formatted 5-day forecast, for anywhere the user chooses. Ask the user for the location.
+
+Make sure your API key is not coded into your program. Your program should read the key from an environment variable. 
+
+Use a query parameter dictionary in the request.
+
+Your forecast should show the temperature and unit (your choice of F or C), weather description, and wind speed for every three hour interval, over the next 5 days.
+
+Your program should handle errors. What type of errors do you anticipate? How will you deal with them?
+
+Part 2: Time choice"""
